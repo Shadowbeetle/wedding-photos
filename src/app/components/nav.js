02d865474363a88ba7de0d715041f9cf0cea5dc6
Shadowbeetle <tamas.kadlecsik@gmail.com>
2017-07-22 21:01:42 +0200

    make header responsive

--- src/app/components/nav.js
+++ src/app/components/nav.js
@@ -15,34 +15,42 @@ class Nav extends Component {
   render () {
     const { activePage, lang } = this.props
     return (
-      <nav className="navbar navbar-default navbar-fixed-top wedding-navbar">
-        <div className="container">
-          <div className="navbar-header">
-            <a className="navbar-brand" href={`?lang=${lang}`}>{locale.title[ lang ]}</a>
+      <nav className="navbar navbar-default navbar-fixed-top navbar-toggleable-md wedding-navbar">
+        <button id="navbar-toggle-button" type="button" className="navbar-toggle" data-toggle="collapse"
+                data-target="#navbar-content">
+          <span className="icon-bar"></span>
+          <span className="icon-bar"></span>
+          <span className="icon-bar"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbar-content">
+          <div className="container">
+            <div className="navbar-header">
+              <a className="navbar-brand" href={`?lang=${lang}`}>{locale.title[ lang ]}</a>
+            </div>
+            <ul className="nav navbar-nav">
+              <li className="nav-item">
+                <a className={classNames('nav-link wedding-navigate', { 'active': activePage === 'photos' })}
+                   data-toggle="tab"
+                   onClick={this.navigate}
+                   href="/photos"
+                   name="photos">
+                  {locale.photos[ lang ]}
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className={classNames('nav-link wedding-navigate', { 'active': activePage === 'videos' })}
+                   data-toggle="tab"
+                   onClick={this.navigate}
+                   href="/videos"
+                   name="videos">
+                  {locale.videos[ lang ]}
+                </a>
+              </li>
+            </ul>
+            <ul className="nav navbar-nav navbar-right">
+              <LangFlag targetLang={lang === 'hu' ? 'en' : 'hu'}/>
+            </ul>
           </div>
-          <ul className="nav navbar-nav">
-            <li className="nav-item">
-              <a className={classNames('nav-link wedding-navigate', { 'active': activePage === 'photos' })}
-                 data-toggle="tab"
-                 onClick={this.navigate}
-                 href="/photos"
-                 name="photos">
-                {locale.photos[ lang ]}
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className={classNames('nav-link wedding-navigate', { 'active': activePage === 'videos' })}
-                 data-toggle="tab"
-                 onClick={this.navigate}
-                 href="/videos"
-                 name="videos">
-                {locale.videos[ lang ]}
-              </a>
-            </li>
-          </ul>
-          <ul className="nav navbar-nav navbar-right">
-            <LangFlag targetLang={lang === 'hu' ? 'en' : 'hu'}/>
-          </ul>
         </div>
       </nav>
     )
