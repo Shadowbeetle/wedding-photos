56f0570aabbcc7cc416ca9bf62b2e2cbc6b99bd8
Shadowbeetle <tamas.kadlecsik@gmail.com>
2017-07-23 18:23:45 +0200

    shrink large photos too

--- script/upload-to-s3.sh
+++ script/upload-to-s3.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
 > server/models/metadata/photos.yaml
 
-all=$(ls images/large | wc -l | tr -d '[:space:]')
+all=$(ls images/original | wc -l | tr -d '[:space:]')
 c=0
-for file in images/large/*; do
+for file in images/original/*; do
   dimensions=$(identify -format "%[fx:w]x%[fx:h]" "$file")
   width=$(tr 'x' '\n' <<< $dimensions | head -n 1)
   height=$(tr 'x' '\n' <<< $dimensions | tail -n 1)
   thumbnail_target_height=250
+  large_target_height=850
   thumbnail_target_width=$(bc <<< "$width*$thumbnail_target_height/$height")
+  large_target_width=$(bc <<< "$width*$large_target_height/$height")
   filename=$(basename "$file")
   thumbnail_path="images/thumbnails/$filename"
+  large_path="images/large/$filename"
   convert "$file" -resize "$thumbnail_target_width"x"$thumbnail_target_width" "$thumbnail_path"
+  convert "$file" -resize "$large_target_width"x"$large_target_width" "$large_path"
   thumbnail_dimensions=$(identify -format "%[fx:w]x%[fx:h]" "$thumbnail_path")
   thumbnail_width=$(tr 'x' '\n' <<< $thumbnail_dimensions | head -n 1)
   thumbnail_height=$(tr 'x' '\n' <<< $thumbnail_dimensions | tail -n 1)
