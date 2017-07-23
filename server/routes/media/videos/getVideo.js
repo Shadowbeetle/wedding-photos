69f8950ae73bd3d2336d8076e21e66acf8620c94
Shadowbeetle <tamas.kadlecsik@gmail.com>
2017-07-23 19:28:17 +0200

    fix video download zise

--- server/routes/media/videos/getVideo.js
+++ server/routes/media/videos/getVideo.js
@@ -2,7 +2,13 @@
 const path = require('path')
 module.exports = function getAllPhotoNames (models, req, res) {
   const name = path.basename(req.params.videoKey)
-  res.attachment(name)
-  models.weddingMedia.getMediaByKey(req.params.videoKey)
-    .pipe(res)
+  models.weddingMedia.getMediaDataByKey(req.params.videoKey)
+    .then((data) => {
+      res.set({
+        'content-length': data.ContentLength
+      })
+      res.attachment(name)
+      models.weddingMedia.getMediaByKey(req.params.videoKey)
+        .pipe(res)
+    })
 }
