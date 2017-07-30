ded8c19d929aa9c6b2ef3092e8d37b8cf8b1ca48
Shadowbeetle <tamas.kadlecsik@gmail.com>
2017-07-30 14:08:18 +0200

    set cache-control headers on photos

--- server/routes/media/photos/getPhoto.js
+++ server/routes/media/photos/getPhoto.js
@@ -1,5 +1,10 @@
 'use strict'
 module.exports = function getAllPhotoNames (models, req, res) {
+  res.set({
+    "Cache-Control": "public, max-age=2592000",
+    ETag: req.params.photoKey,
+    Expires: new Date(Date.now() + 2592000000).toUTCString()
+  })
   models.weddingMedia.getMediaByKey(req.params.photoKey)
     .pipe(res)
 }
